9ac6c973242e6d51b9ac19a1c7674095b0b1c7f1
Ahmet Yaşar Özer <ahmetozer10@gmail.com>
2025-10-07 11:30:57 +0300

    ...

--- src/app/lifecycle/lifecycle.component.ts
+++ src/app/lifecycle/lifecycle.component.ts
@@ -36,36 +36,43 @@ export class LifecycleComponent
     console.log('CONSTRUCTOR');
   }
 
-  ngOnInit() { // Runs once after Angular has initialized all the component's inputs.
-    console.log('ngOnInit');
-  }
-
   ngOnChanges(changes: SimpleChanges) { // Runs every time the component's inputs have changed.
-    // Suggested not to be implemented, as it is called a lot of times while applcation is running.
     console.log('ngOnChanges');
     console.log(changes);
   }
 
-  ngDoCheck() { // Runs every time this component is checked for changes.
+  ngOnInit() { // Runs once after Angular has initialized all the component's inputs.
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck() { // Runs every time this component's template is checked for changes.
+    // Suggested not to be implemented, as it is called a lot of times while applcation is running.
     console.log('ngDoCheck');
   }
 
-  ngAfterContentInit() { // Runs once after the component's content (projected content - ng-content) 
+  ngAfterContentInit() { // Runs once after the component's content (children nested inside 
+  // this component - projected content - ng-content) 
   // has been initialized.
+  // Can be used to read the results of content queries (contentChild or contentChildren).
     console.log('ngAfterContentInit');
   }
 
   ngAfterContentChecked() { // Runs every time the component's content has been checked for changes.
+    // Suggested not to be implemented, as it is called a lot of times while applcation is running.
     console.log('ngAfterContentChecked');
   }
 
   ngAfterViewInit() { // Runs once after the component's view (template) has been  initialized.
+    // Can be use to read the results of view queries (viewChild or viewChildren)
     console.log('ngAfterViewInit');
   }
 
   ngAfterViewChecked() { // Runs every time the component's view has been checked for changes.
+    // Suggested not to be implemented, as it is called a lot of times while applcation is running.
     console.log('ngAfterViewChecked');
   }
+  // Attempting to modify states in ngAfterContentInit, ngAfterContentChecked, ngAfterViewInit and
+  // ngAfterViewChecked will result in ExpressionChangedAfterItHasBeenCheckedError error.
 
   ngOnDestroy() { // Runs once before the component is destroyed.
     console.log('ngOnDestroy');
